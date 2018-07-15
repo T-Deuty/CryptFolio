f4f70ad629c4b852b6ead78c2c29ba80808d5a97
Tyler Albert Louis Deuty <tdeuty@asu.edu>
2018-07-15 11:32:55 -0600

    Using XAML templates for dispalying market currencies

---
+++ CryptFolio-UWP/Helper Classes/MarketViewTemplate.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptFolio
+{
+    public class MarketViewTemplate
+    {
+        public string currencyName { get; set; }
+        public string currencyUSDPrice { get; set; }
+        public string currencyBTCPrice { get; set; }
+        public string currencyMarketCap { get; set; }
+        public string currencyCirculatingSupply { get; set; }
+        public string currencyTotalSupply { get; set; }
+        public string currencyPercentChange1hr { get; set; }
+        public string currencyPercentChange24hr { get; set; }
+        public string currencyPercentChange1wk { get; set; }
+
+        public MarketViewTemplate(
+            string currencyName, 
+            string currencyUSDPrice,
+            string currencyBTCPrice,
+            string currencyMarketCap,
+            string currencyCirculatingSupply,
+            string currencyTotalSupply,
+            string currencyPercentChange1hr,
+            string currencyPercentChange24hr,
+            string currencyPercentChange1wk
+            )
+        {
+            this.currencyName = currencyName;
+            this.currencyUSDPrice = "USD Price: $" + currencyUSDPrice;
+            this.currencyBTCPrice = "BTC Price: " + currencyBTCPrice;
+            this.currencyMarketCap = "Market Cap: $" + currencyMarketCap;
+            this.currencyCirculatingSupply = "Circulating supply: " + currencyCirculatingSupply;
+            this.currencyTotalSupply = "Total supply: " + currencyTotalSupply;
+            this.currencyPercentChange1hr = "Percent Change (1hr): " + currencyPercentChange1hr;
+            this.currencyPercentChange24hr = "Percent Change (24hr): " + currencyPercentChange24hr;
+            this.currencyPercentChange1wk = "Percent Change (1 week): " + currencyPercentChange1wk;
+        }
+    }
+
+    public class MarketViewTemplates : ObservableCollection<MarketViewTemplate>
+    {
+        public MarketViewTemplates() : base()
+        {
+            foreach (TickerJSONResult result in App.jsonList)
+            {
+                Add(new MarketViewTemplate
+                        (
+                            result.name, 
+                            result.price_usd,
+                            result.price_btc,
+                            result.market_cap_usd,
+                            result.available_supply,
+                            result.max_supply,
+                            result.percent_change_1h,
+                            result.percent_change_24h,
+                            result.percent_change_7d
+                        )
+                    );
+            }
+        }
+    }
+}
